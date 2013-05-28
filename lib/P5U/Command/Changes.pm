2ce9059c7d4fc18c9c311c6e9bcaa2acde6032a4
Toby Inkster <mail@tobyinkster.co.uk>
2013-05-28 20:09:37 +0100

    bump version; changelog

--- lib/P5U/Command/Changes.pm
+++ lib/P5U/Command/Changes.pm
@@ -9,7 +9,7 @@ use match::simple;
 
 BEGIN {
 	$P5U::Command::Changes::AUTHORITY = 'cpan:TOBYINK';
-	$P5U::Command::Changes::VERSION   = '0.001';
+	$P5U::Command::Changes::VERSION   = '0.002';
 }
 
 use constant {
@@ -176,7 +176,7 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2012 by Toby Inkster.
+This software is copyright (c) 2012-2013 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
